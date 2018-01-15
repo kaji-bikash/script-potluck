6966160ce9d2f8be2977a653b53f6babf25628b6
Kaji Bikash <kajisaap@gmail.com>
2018-01-14 23:50:38 -0500

    Simple public key appender to SSH authorized keys with many assumptions

---
+++ pubkey_appender.sh
@@ -0,0 +1,22 @@
+#! /bin/bash -xv
+#Assumptions
+#Default user deploy whose SSH authorized_keys is to modify
+#SSH authorized_keys is in the format ssh-rsa key 
+home=${home:-deploy}
+username=$1
+
+#get public key from a github
+KEY="https://github.com/$username.keys"
+curl --silent $KEY -o /tmp/$username.pub
+
+ssh_auth_file=`eval echo ~${home}/.ssh/authorized_keys`
+
+##Append found keys to authorized_keys file
+while read line
+do
+echo $line >> ${ssh_auth_file}
+done <"/tmp/${username}.pub"
+
+#Simulate idempotence/de-duplication
+cat $ssh_auth_file | sort -u -k1,2 > ${ssh_auth_file}.uniq
+mv ${ssh_auth_file}.uniq ${ssh_auth_file}
