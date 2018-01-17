3f97bc5f69f1196cbd23ee95d8f189ff07417325
Kaji Bikash <kajisaap@gmail.com>
2018-01-17 13:47:13 -0500

    Added useful warning and apt notification on output

--- pubkey_appender.sh
+++ pubkey_appender.sh
@@ -12,6 +12,14 @@ fi
 #SSH authorized_keys is in the format ssh-rsa key 
 home=${home:-deploy}
 username=$1
+name=`curl --silent https://api.github.com/users/kajisaap | grep "name" | awk -F: '{ print $2}' | cut -d "," -f1`
+
+prompt() {
+echo "You are adding $username - $name"
+read -p "To avoid unintended pubkey additions, press ENTER/RETURN to continue: "
+}
+
+prompt;
 
 #get public key from a github
 KEY="https://github.com/$username.keys"
@@ -28,3 +36,11 @@ done <"/tmp/${username}.pub"
 #Simulate idempotence/de-duplication
 cat $ssh_auth_file | sort -u -k1,2 > ${ssh_auth_file}.uniq
 mv ${ssh_auth_file}.uniq ${ssh_auth_file}
+
+
+
+if [ $? -eq "0" ]; then
+echo "SUCCESS : $name - $username added"; exit 0;
+fi
+
+echo "FAILED: $name - $username not added"; exit 1;
