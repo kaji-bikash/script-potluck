4f3442da46e8e0b74e68e3ae414a985b7348eca5
Kaji Bikash <kajisaap@gmail.com>
2018-01-17 18:16:54 -0500

    refactor and some output modification

--- pubkey_appender.sh
+++ pubkey_appender.sh
@@ -9,15 +9,16 @@ fi
 
 #Assumptions
 #Default user deploy whose SSH authorized_keys is to modify
-#SSH authorized_keys is in the format ssh-rsa key 
+#SSH authorized_keys is in the format ssh-rsa key
 home=${home:-deploy}
 username=$1
 name=`curl --silent https://api.github.com/users/$username | grep "name" | awk -F: '{ print $2}' | cut -d "," -f1`
 
 prompt() {
+echo ""
 echo "You are adding $username - $name"
 
-read -n1 -r -p "To avoid unintended pubkey additions, press any key to continue..." key
+read -n1 -r -p "To avoid unintended additions, press any key to continue..." key
 
 #if ! [ "$key" = "y" ]; then
 #    exit 1;
@@ -35,7 +36,7 @@ ssh_auth_file=`eval echo ~${home}/.ssh/authorized_keys`
 ##Append found keys to authorized_keys file
 while read line
 do
-echo "$line ${username} ${user}" >> ${ssh_auth_file}
+echo "$line ${username}" >> ${ssh_auth_file}
 done <"/tmp/${username}.pub"
 
 #Simulate idempotence/de-duplication
@@ -45,7 +46,8 @@ mv ${ssh_auth_file}.uniq ${ssh_auth_file}
 
 
 if [ $? -eq "0" ]; then
+echo ""
 echo "SUCCESS : $name - $username added"; exit 0;
 fi
-
+echo ""
 echo "FAILED: $name - $username not added"; exit 1;
